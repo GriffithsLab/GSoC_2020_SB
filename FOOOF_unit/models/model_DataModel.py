be5e83265a294ac425ee7613aec42519366a5b54
sbastiaens <sorenza.bastiaens@gmail.com>
2020-06-21 12:38:52 +0000

    model_DataModel.py

---
+++ FOOOF_unit/models/model_DataModel.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Neural Power Spectra SciUnit data class"""
+
+class DataModel(sciunit.Model,ProducesPowerSpectrum):
+  """Model that creates a frozen data trace of the neural power spectrum of the data."""
+  def __init__(self, freqs, powers, freq_range, name=None):
+        self.freqs = freqs
+        self.powers = powers
+        self.freq_range = freq_range
+        super(DataModel, self).__init__(name=name, freqs=freqs, powers=powers, freq_range=freq_range)
+
+  def produce_power_spectrum(self):
+        return (self.freqs, self.powers, self.freq_range)

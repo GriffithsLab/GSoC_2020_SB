88da52f6d76b9f679182fd34f0fca992fb0fbfd3
sbastiaens <62792658+sbastiaens@users.noreply.github.com>
2020-06-23 21:06:58 +0200

    Update test_band_by_band.py
    @rgerkin thank you for your reply. I changed the test according to the example you gave me. _Band_by_band is now the abstract class and the two tests are PeakPower and BandPower. Is this a correct implementation of the test? Does it corresponds to what you were thinking of?

--- FOOOF_unit/tests/test_band_by_band.py
+++ FOOOF_unit/tests/test_band_by_band.py
@@ -10,37 +10,21 @@ from fooof.utils import trim_spectrum
 from sciunit.scores import FloatScore
 from capabilities.cap_ProducesPowerSpectrum import ProducesPowerSpectrum
 
+    
+class _Band_by_Band(sciunit.Test):
+  """Test comparing the power of detected peaks or of frequency band ranges of the observation and the prediction model  """
 
-#FOOOF helper functions
-
-def compare_peak_pw(fm1, fm2, band_def):
-    """Compare the power of detected peaks."""
-
-    pw1 = get_band_peak_fm(fm1, band_def)[1]
-    pw2 = get_band_peak_fm(fm2, band_def)[1]
-
-    return pw1 - pw2
-
-def compare_band_pw(fm1, fm2, band_def):
-    """Compare the power of frequency band ranges."""
-
-    pw1 = np.mean(trim_spectrum(fm1.freqs, fm1.power_spectrum, band_def)[1])
-    pw2 = np.mean(trim_spectrum(fm1.freqs, fm2.power_spectrum, band_def)[1])
-
-    return pw1 - pw2
-
-#Test class: Have the option to select compare_peak_pw by intializing it with option=1. By default, compare_band_pw is used.
-class Band_by_Band(sciunit.Test):
-  """Test giving a FloatScore which compares the power of frequency band ranges of the observation and the prediction model  """
-
-  def __init__(self, observation=None, name=None, band=None, option=None):
-    super().__init__(observation=observation, name=name, band=band, option=option)
+  def __init__(self, observation=None, name=None, band=None):
+    super().__init__(observation=observation, name=name, band=band)
     self.band = band
-    self.option = option
 
   required_capabilities = (ProducesPowerSpectrum,)
   score_type = FloatScore
-
+    
+  def compare(self, fm1, fm2, band_def):
+    """Implemented in the subclasses"""
+    return NotImplementedError()
+    
   def generate_prediction(self, model):
     res = model.produce_power_spectrum()
     frequency = res[0]
@@ -57,10 +41,28 @@ class Band_by_Band(sciunit.Test):
     fm_pred.fit(prediction['freqs'], prediction['powers'], prediction['freq_range'])
 
     bands = Bands(self.band)
-    if (self.option == 1):
-      for label, definition in bands:
-        score = self.score_type((compare_peak_pw(fm_pred, fm_obs, definition)))
-    else:
-      for label, definition in bands:
-        score = self.score_type((compare_band_pw(fm_pred, fm_obs, definition)))
+    for label, definition in bands:
+      score = self.score_type((self.compare(fm_pred, fm_obs, definition)))
     return score
+
+
+class PeakPower(_Band_by_Band):
+
+  def compare(self, fm1, fm2, band_def):
+    """Compare the power of detected peaks."""
+
+    pw1 = get_band_peak_fm(fm1, band_def)[1]
+    pw2 = get_band_peak_fm(fm2, band_def)[1]
+
+    return pw1 - pw2
+
+
+class BandPower(_Band_by_Band):
+
+  def compare(self, fm1, fm2, band_def):
+    """Compare the power of frequency band ranges."""
+
+    pw1 = np.mean(trim_spectrum(fm1.freqs, fm1.power_spectrum, band_def)[1])
+    pw2 = np.mean(trim_spectrum(fm1.freqs, fm2.power_spectrum, band_def)[1])
+
+    return pw1 - pw2

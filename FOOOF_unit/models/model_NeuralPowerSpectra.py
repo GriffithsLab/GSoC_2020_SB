e2d6b6b5ad8d3d130cb6917d1b8b228f12e35a2a
sbastiaens <62792658+sbastiaens@users.noreply.github.com>
2020-06-23 18:41:55 +0200

    Update model_NeuralPowerSpectra.py

--- FOOOF_unit/models/model_NeuralPowerSpectra.py
+++ FOOOF_unit/models/model_NeuralPowerSpectra.py
@@ -2,7 +2,7 @@
 """Neural Power Spectra SciUnit model class"""
 
 import sciunit
-from capabilities.cap_ProducesPowerSpectrum import ProducesPowerSpectrum
+from FOOOF_unit.capabilities.cap_ProducesPowerSpectrum import ProducesPowerSpectrum
 
 
 class NeuralPowerSpectra(sciunit.Model, ProducesPowerSpectrum):

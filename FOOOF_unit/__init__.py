4eced45a69d378862cfcb0420c544ce71891206f
sbastiaens <sorenza.bastiaens@gmail.com>
2020-07-24 14:07:31 +0000

    Package files

---
+++ FOOOF_unit/__init__.py
@@ -0,0 +1,7 @@
+"""FOOOF_Unit SciUnit library tests validating features of models simulating Neural Power Spectrum"""
+
+from . import capabilities, models, scores, tests
+from .utils import common_fr_bands
+from .io import load_data_mat, visualize_multiple_data
+
+__version__ = "0.0.1"
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="FOOOF_unit", 
+    version="0.0.1",
+    author="Sorenza Bastiaens",
+    author_email="sorenza.bastiaens@gmail.com",
+    description="A SciUnit library to test features of simulated neural power spectra",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/",  #Need to fill in
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

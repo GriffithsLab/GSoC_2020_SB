3ee28926be1730f5713d1f7d61352a0586447c5b
sbastiaens <sorenza.bastiaens@gmail.com>
2020-07-30 14:53:59 +0000

    test for Jupyter notebooks

---
+++ test_jnotebooks.py
@@ -0,0 +1,19 @@
+import subprocess
+import tempfile
+import os
+
+def _exec_notebook(path):
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=None",
+                "--output", fout.name, path]
+        subprocess.check_call(args)
+
+
+def test():
+  name_examples = os.listdir('examples')
+  for ex in name_examples:
+    _exec_notebook('examples/'+ex)
+    
+"""Option 2"""
+#  jupyter nbconvert --ExecutePreprocessor.timeout=1000 --to notebook --execute examples/Example1_CentralPeak.ipynb

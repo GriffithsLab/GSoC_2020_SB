b94ef2e8d8e008a3340b2ec834b0c2fede086809
sbastiaens <62792658+sbastiaens@users.noreply.github.com>
2020-08-30 19:51:50 +0200

    Update __init__.py

--- FOOOF_unit/tests/__init__.py
+++ FOOOF_unit/tests/__init__.py
@@ -1,5 +1,5 @@
 """Test classes for FOOOF_unit"""
 
-from .test_centralpeak import CentralPeak
-from .test_band_by_band import BandPower, PeakPower
-from .test_correlation2spectrum import Correlation2Spectrum, Correlation2SpectrumFloat
+from .centralpeak import CentralPeak
+from .band_by_band import BandPower, PeakPower
+from .correlation2spectrum import Correlation2Spectrum, Correlation2SpectrumFloat
